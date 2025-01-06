41bc72409f19ab3d00b565b5bb017730c92b372e
skeo <skeo051600@gmail.com>
2025-01-06 12:12:00 -0600

    Add (incomplete) computer input

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 
-let grid = [
-  [[], [], []],
-  [[], [], []],
-  [[], [], []]
-];
-
-let playerTurn = true;
-
 const gameContainer = document.getElementById("game-container");
 
 const a1 = document.getElementById("a1")
@@ -21,15 +13,29 @@ const c1 = document.getElementById("c1")
 const c2 = document.getElementById("c2")
 const c3 = document.getElementById("c3")
 
+let grid = [
+  [[], [], []],
+  [[], [], []],
+  [[], [], []]
+];
+
+const idMap = [
+  ['a1', 'b1', 'c1'],
+  ['a2', 'b2', 'c2'],
+  ['a3', 'b3', 'c3']
+];
+
+let playerTurn = true;
+
 gameContainer.addEventListener('click', (event) => {
   const targetId = event.target.id;
   if (playerTurn) {
-    if (targetId === 'a1') {
+    if (targetId === 'a1' && ((grid[0][0]).length === 0)) {
       grid[0][0].push("X")
       a1.innerHTML = 'x';
       (playerTurn = !playerTurn)
-      if (gameCheck = false) {
-        // add a function that picks a random thing, then allows the player to choose again
+      if (gameCheck() === false) {
+        setTimeout(computerOpponent, 500);
       } else {
         // function that resets the board
       }
@@ -73,7 +79,31 @@ const gameCheck = () => {
   return false; // game ongoing
 }
 
+const computerOpponent = () => {
+  let computerOpponentRow = (Math.floor(Math.random() * 3))
+  let computerOpponentColumn = (Math.floor(Math.random() * 3))
 
+  const computerOpponentInput = (computerOpponentRow, computerOpponentColumn) => {
+    if (grid[computerOpponentRow][computerOpponentColumn].length === 0) {
+      grid[computerOpponentRow][computerOpponentColumn].push('O')
+
+      document.getElementById(idMap[computerOpponentRow][computerOpponentColumn]).innerHTML = 'o'
+    } else {
+      computerOpponent()
+    }
+  }
+
+  computerOpponentInput(computerOpponentRow, computerOpponentColumn);
+  (playerTurn = !playerTurn)
+}
+
+document.addEventListener("click", () => {
+  console.log(grid)
+  console.log(playerTurn)
+})
+
+// so now we have 2 random numbers we can use for row and columns
+// create a function that would take those 2 inputs
 
 // a1.addEventListener("click", () => { console.log("a1 clicked!!") })
 // a2.addEventListener("click", () => { console.log("a2 clicked!!") })
@@ -86,13 +116,3 @@ const gameCheck = () => {
 // c1.addEventListener("click", () => { console.log("c1 clicked!!") })
 // c2.addEventListener("click", () => { console.log("c2 clicked!!") })
 // c3.addEventListener("click", () => { console.log("c3 clicked!!") })
-
-
-// if a1 is clicked, then i want [0, 0] to be true
-// if a1 has a click event
-// replace the console log with grid [0, 0] = 1
-// console.log grid
-
-
-// 1 is user
-// 2 is computer 
